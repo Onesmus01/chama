23c18562dd3c8165c0934881d4d193cd5445bec0
Onesmus01 <145201137+Onesmus01@users.noreply.github.com>
2025-04-22 05:43:26 +0300

    added

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "http://localhost:4173", 
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['chama-7.onrender.com'],
+  },
   server: {
-    port: '5173',
+    host: '0.0.0.0', // Allow external access
+    port: 4173, // Your preferred port
     proxy: {
-      '/api': 'http://localhost:6500'
+      '/api': 'http://localhost:6500', // Ensure this is the correct API URL in your Render deployment
     }
   }
 })
- 
