44bc12d23c12323c7c03c8f932fc555a068b1a2b
unknown <onesmuswambua747@gmail.com>
2025-05-10 13:30:30 +0300

    new deployments

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/chama",
   preview: {
     allowedHosts: ['chama-7.onrender.com'],
   },
